0e0a627b12cdb07ddd4061c79313589846ad241f
Tagu <tiago.venus@hotmail.com>
2026-06-09 01:12:39 -0300

    teste para compatibilidade bluetooth de impressao

--- js/pages/vendas.page.js
+++ js/pages/vendas.page.js
@@ -290,7 +290,7 @@ function abrirModalVenda(venda){
 }
 
 async function compartilharVenda(venda){
-    alert(typeof navigator.bluetooth)
+    alert("teste:", typeof navigator.bluetooth)
     try{
 
         await navigator.share({
