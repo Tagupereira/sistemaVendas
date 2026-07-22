f5e0a424145bc57376d9c5e01d6ae942c08a2e92
Tagu <tiago.venus@hotmail.com>
2026-07-22 01:48:36 -0300

    adiciona toast de ações na pagina de dados empresa

--- js/pages/configConta.page.js
+++ js/pages/configConta.page.js
@@ -1,8 +1,12 @@
 import { reqCount } from "../api/requestDados.api.js";
 import { go } from '../routes/routes.js';
+import { startLoading, stopLoading, showLoading, hideLoading } from '../components/loading.component.js';
+import { toast } from "../components/toast.component.js";
 
 const buscarDados = document.getElementById("buscarDados");
 const campoEmpresa = document.getElementById("campoEmpresa");
+const gravarDados = document.getElementById("gravarDados");
+
 
 let empresa = null;
 
@@ -11,53 +15,103 @@ document.getElementById("back").addEventListener("click",()=>{
 })
 
 buscarDados.addEventListener("click",async ()=>{
+    toast("Buscando aguarde...", "warning")
 
     const id = document.getElementById("pesquisa").value;
         
     const dadoReq = await reqCount('buscarEmpresa', id);
 
     empresa = dadoReq.empresa;
-
+    
+    if(empresa === undefined){
+        toast("Nada encontrado", "error");
+        return;
+    }
+    
     const nome = empresa.nome.toUpperCase()
-
+    
     campoEmpresa.innerHTML = `
-      <div class="bg-white rounded-3xl shadow p-4 flex flex-column justify-between items-center">
-        <div class="">
+        <div class="bg-white rounded-3xl shadow p-4 flex flex-column justify-between items-center">
+            <div class="">
 
-            <p class="text-xs text-gray-500 ">
-                ID: ${empresa.id}
-            </p>
+                <p class="text-xs text-gray-500 ">
+                    ID: ${empresa.id}
+                </p>
 
-            <h3 class="font-bold text-base text-gray-800 truncate">
-                ${nome}
-            </h3>
+                <h3 class="font-bold text-base text-gray-800 truncate">
+                    ${nome}
+                </h3>
 
-            <p class="text-base text-gray-800 mt-0.5 truncate">
-                Licenca: ${empresa.licenca}
-            </p>
+                <p class="text-base text-gray-800 mt-0.5 truncate">
+                    Licenca: ${empresa.licenca}
+                </p>
 
-            <span class="text-base text-gray-800 mt-0.5 truncate">
-                Status: ${empresa.status}
-            </span>
+                <span class="text-base text-gray-800 mt-0.5 truncate">
+                    Status: ${empresa.status}
+                </span>
 
+            </div>
         </div>
-      </div>
-      <button id="gravarDados" class="w-full bg-blue-500 text-white rounded-xl p-3 mt-5 mb-5">
+        <button id="gravarDados" class="w-full bg-blue-500 text-white rounded-xl p-3 mt-5 mb-5">
 
             Gravar Dados
 
         </button>
     `;
-
     const gravarDados = document.getElementById("gravarDados");
-
     gravarDados.addEventListener("click",()=>{        
         gravar(empresa);
+        gravarDados.classList.add("hidden");
     })
 
   });
 
 function gravar(empresa){
-      
+
+    
+    toast("Cadastrando...","warning");
+
     localStorage.setItem("empresa",JSON.stringify(empresa))
 }
+
+function confirmaEmpresa(){
+        
+    const empresa = JSON.parse(localStorage.getItem("empresa"));
+
+    if(empresa){
+        
+        const nome = empresa.nome.toUpperCase()
+
+        campoEmpresa.innerHTML = `
+        <div class="bg-white rounded-3xl shadow p-4 flex flex-column justify-between items-center">
+            <div class="">
+
+                <p class="text-xs text-gray-500 ">
+                    ID: ${empresa.id}
+                </p>
+
+                <h3 class="font-bold text-base text-gray-800 truncate">
+                    ${nome}
+                </h3>
+
+                <p class="text-base text-gray-800 mt-0.5 truncate">
+                    Licenca: ${empresa.licenca}
+                </p>
+
+                <span class="text-base text-gray-800 mt-0.5 truncate">
+                    Status: ${empresa.status}
+                </span>
+
+            </div>
+        </div>
+    `;
+    
+    }else{
+        campoEmpresa.innerHTML = `
+            <div class="bg-white rounded-3xl shadow p-4 flex flex-column justify-between items-center">
+                <div class="w-[100%] h-[100px] flex text-center justify-center items-center text-slate-500">Nenhuma empresa cadastrada.</div>
+            </div>
+        `;
+    }
+}
+confirmaEmpresa();
