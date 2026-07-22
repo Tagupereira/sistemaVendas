e8bcafe4456856bf9eccb279ece89c1e45482708
Tagu <tiago.venus@hotmail.com>
2026-07-22 19:42:31 -0300

    adiciona privacidade ai adicionar os dados da empresa

--- js/pages/configConta.page.js
+++ js/pages/configConta.page.js
@@ -6,7 +6,7 @@ import { toast } from "../components/toast.component.js";
 const buscarDados = document.getElementById("buscarDados");
 const campoEmpresa = document.getElementById("campoEmpresa");
 const gravarDados = document.getElementById("gravarDados");
-
+const pesquisa = document.getElementById("campoPesquisa");
 
 let empresa = null;
 
@@ -32,8 +32,8 @@ buscarDados.addEventListener("click",async ()=>{
     
     campoEmpresa.innerHTML = `
         <div class="bg-white rounded-3xl shadow p-4 flex flex-column justify-between items-center">
-            <div class="">
 
+            <div class="">
                 <p class="text-xs text-gray-500 ">
                     ID: ${empresa.id}
                 </p>
@@ -58,17 +58,47 @@ buscarDados.addEventListener("click",async ()=>{
 
         </button>
     `;
+
     const gravarDados = document.getElementById("gravarDados");
+    const buscarDados = document.getElementById("buscarDados");
+    const pesquisa = document.getElementById("campoPesquisa");
     gravarDados.addEventListener("click",()=>{        
         gravar(empresa);
         gravarDados.classList.add("hidden");
+        buscarDados.classList.add("hidden");
+        pesquisa.classList.add("hidden");   
+
+        
+        campoEmpresa.innerHTML = `
+        <h1 class="m-5 font-bold text-gray-600">Registrado para:</h1>
+        <div class="bg-white rounded-3xl shadow p-4 flex flex-column justify-between items-center">
+            <div class="">
+                <p class="text-xs text-gray-500 ">
+                    ID: ${empresa.id}
+                </p>
+
+                <h3 class="font-bold text-base text-gray-800 truncate">
+                    ${nome}
+                </h3>
+
+                <p class="text-base text-gray-800 mt-0.5 truncate">
+                    Licenca: ${empresa.licenca}
+                </p>
+
+                <span class="text-base text-gray-800 mt-0.5 truncate">
+                    Status: ${empresa.status}
+                </span>
+
+            </div>
+        </div>
+    `;
+
     })
 
   });
 
 function gravar(empresa){
 
-    
     toast("Cadastrando...","warning");
 
     localStorage.setItem("empresa",JSON.stringify(empresa))
@@ -79,11 +109,14 @@ function confirmaEmpresa(){
     const empresa = JSON.parse(localStorage.getItem("empresa"));
 
     if(empresa){
-        
+        buscarDados.classList.add("hidden");
+        pesquisa.classList.add("hidden");
         const nome = empresa.nome.toUpperCase()
 
         campoEmpresa.innerHTML = `
+        <h1 class="m-5 font-bold text-gray-600">Registrado para:</h1>
         <div class="bg-white rounded-3xl shadow p-4 flex flex-column justify-between items-center">
+        
             <div class="">
 
                 <p class="text-xs text-gray-500 ">
